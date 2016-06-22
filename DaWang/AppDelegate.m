74bce8b3932e0d5907670554edd80f4d9547e97f
lifei521898 <li331416654@163.com>
2016-06-22 14:13:43 +0800

    1111
    1111

--- DaWang/AppDelegate.m
+++ DaWang/AppDelegate.m
@@ -18,8 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    UIImage * img;
-   
+  
     return YES;
 }
 
