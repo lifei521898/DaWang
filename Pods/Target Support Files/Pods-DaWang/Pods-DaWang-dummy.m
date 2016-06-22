75788a8e70096f29567a8a4f1feeac11a261faf1
lifei521898 <li331416654@163.com>
2016-06-22 15:23:52 +0800

    1211111
    1111

--- Pods/Target Support Files/Pods-DaWang/Pods-DaWang-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DaWang : NSObject
-@end
-@implementation PodsDummy_Pods_DaWang
-@end
--- Pods/Target Support Files/Pods-DaWangUITests/Pods-DaWangUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DaWangUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DaWangUITests
-@end
